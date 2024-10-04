979f161003559f0ffdf25a22438b70250dec4ff0
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2024-10-04 15:11:51 +0900

    테스트 코드 추가2

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -2,8 +2,15 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-
+from django.urls import reverse
 from .models import Question
+
+def create_question(question_text, days):
+    """
+    
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
